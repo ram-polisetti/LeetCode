c13e207173144c706eb98bcc6c6794132d200bd2
ramCharan polisetti <saitejapolisetti284@gmail.com>
2023-12-24 17:53:05 -0500

    Time: 439 ms (31.58%), Space: 29.1 MB (52.54%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,9 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        nums_len = len(set(nums))
-        if len(nums) != nums_len:
-            return True
-        else:
-            return False
+        # nums_len = len(set(nums))
+        # if len(nums) != nums_len:
+        #     return True
+        # else:
+        #     return False
         
+        hashset = set()
+        for ele in nums:
+            if ele in hashset:
+                return True
+            hashset.add(ele)
+        return False
+            
