31dc2c6bb5e041dac19459e6bc664c43f764644e
ramCharan polisetti <saitejapolisetti284@gmail.com>
2024-01-02 19:58:25 +0530

    Time: 156 ms (5.13%) | Memory: 13.6 MB (22.53%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,24 +1,31 @@
+# class Solution(object):
+#     def canConstruct(self, ransomNote, magazine):
+#         """
+#         :type ransomNote: str
+#         :type magazine: str
+#         :rtype: bool
+#         """
+
+#         if len(ransomNote)>len(magazine):
+#             return False
+        
+#         mag_chars = {}
+#         for char in magazine:
+#             if char in mag_chars:
+#                 mag_chars[char] += 1
+#             else:
+#                 mag_chars[char] = 1
+
+#         for char in ransomNote:
+#             if char in mag_chars and mag_chars[char] >0:
+#                 mag_chars[char] -= 1
+#             else:
+#                 return False
+#         return True
+
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
-        """
-        :type ransomNote: str
-        :type magazine: str
-        :rtype: bool
-        """
-
-        if len(ransomNote)>len(magazine):
-            return False
-        
-        mag_chars = {}
-        for char in magazine:
-            if char in mag_chars:
-                mag_chars[char] += 1
-            else:
-                mag_chars[char] = 1
-
-        for char in ransomNote:
-            if char in mag_chars and mag_chars[char] >0:
-                mag_chars[char] -= 1
-            else:
-                return False
-        return True
+        st1, st2 = Counter(ransomNote), Counter(magazine)
+        if st1 & st2 == st1:
+            return True
+        return False
