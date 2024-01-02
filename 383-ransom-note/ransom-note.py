4bd473a585454f88219180cb10457ed6091f7beb
ramCharan polisetti <saitejapolisetti284@gmail.com>
2024-01-02 19:49:13 +0530

    Time: 241 ms (5.13%) | Memory: 13.5 MB (41.88%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+
+        if len(ransomNote)>len(magazine):
+            return False
+        
+        mag_chars = {}
+        for char in magazine:
+            if char in mag_chars:
+                mag_chars[char] += 1
+            else:
+                mag_chars[char] = 1
+        print(mag_chars)
+        for char in ransomNote:
+            if char in mag_chars:
+                mag_chars[char] -= 1
+                print(mag_chars)
+                if mag_chars[char] <0:
+                    return False
+            else:
+                return False
+        return True
