e1afc2fed343161a7faee52568e2759ec7f4d162
ramCharan polisetti <saitejapolisetti284@gmail.com>
2024-01-03 23:32:40 +0530

    Time: 109 ms (97.31%) | Memory: 14.8 MB (88.76%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,13 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        freq = Counter(nums)
-        print(freq)
-        max = 0
-        maj_ele = 0
-        for num in freq:
-            if freq[num] > max and freq[num] > len(nums)//2:
-                max = freq[num]
-                maj_ele = num 
+        # freq = Counter(nums)
+        # print(freq)
+        # max = 0
+        # maj_ele = 0
+        # for num in freq:
+        #     if freq[num] > max and freq[num] > len(nums)//2:
+        #         max = freq[num]
+        #         maj_ele = num 
         
-        return maj_ele
+        # return maj_ele
+
+
+        nums.sort()
+        n = len(nums)//2
+        return nums[n]
