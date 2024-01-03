d2ed1bdf428c3b6b4d3139ffc28668e546274b39
ramCharan polisetti <saitejapolisetti284@gmail.com>
2024-01-03 22:39:44 +0530

    Time: 8 ms (91.70%) | Memory: 13.2 MB (85.31%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # if n==0 or n==1:
+        #     return 1
+        # else:
+        #     return self.climbStairs(n-1) + self.climbStairs(n-2)
+        # This solution works but it is exceeding the time limit
+
+        if n==0 or n==1:
+            return 1
+        temp_arr = [0]*(n+1)
+        temp_arr[0] = temp_arr[1] = 1
+        for i in range(2, n+1):
+            temp_arr[i] = temp_arr[i-1] + temp_arr[i-2]
+        return temp_arr[n]
