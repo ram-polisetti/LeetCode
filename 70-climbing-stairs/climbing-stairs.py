2c296f3dff07051fea43b8f663ab59035c33e7a7
ramCharan polisetti <saitejapolisetti284@gmail.com>
2024-01-03 22:42:08 +0530

    Time: 12 ms (73.97%) | Memory: 13.2 MB (85.31%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -10,10 +10,20 @@ class Solution(object):
         #     return self.climbStairs(n-1) + self.climbStairs(n-2)
         # This solution works but it is exceeding the time limit
 
+        # if n==0 or n==1:
+        #     return 1
+        # temp_arr = [0]*(n+1)
+        # temp_arr[0] = temp_arr[1] = 1
+        # for i in range(2, n+1):
+        #     temp_arr[i] = temp_arr[i-1] + temp_arr[i-2]
+        # return temp_arr[n]
+
+        # We can also do this by just using prev and curr values
         if n==0 or n==1:
             return 1
-        temp_arr = [0]*(n+1)
-        temp_arr[0] = temp_arr[1] = 1
+        prev = curr =1
         for i in range(2, n+1):
-            temp_arr[i] = temp_arr[i-1] + temp_arr[i-2]
-        return temp_arr[n]
+            temp = curr
+            curr = prev + curr
+            prev = temp
+        return curr
