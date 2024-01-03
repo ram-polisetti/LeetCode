f043d54d72992ad449076abffa4c850bb84215dc
ramCharan polisetti <saitejapolisetti284@gmail.com>
2024-01-03 23:31:08 +0530

    Time: 141 ms (12.48%) | Memory: 15.1 MB (29.08%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        freq = Counter(nums)
+        print(freq)
+        max = 0
+        maj_ele = 0
+        for num in freq:
+            if freq[num] > max and freq[num] > len(nums)//2:
+                max = freq[num]
+                maj_ele = num 
+        
+        return maj_ele
