9d466ea12bda9f0fdc02a37db7469f297be3d6b8
ramCharan polisetti <saitejapolisetti284@gmail.com>
2024-01-15 10:00:04 +0530

    Time: 15 ms (77.36%) | Memory: 13.5 MB (16.34%) - LeetSync

---
+++ 67-add-binary/add-binary.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        result = ''
+        carry = 0
+
+        a= list(a)
+        b = list(b)
+
+        while a or b or carry:
+            if a:
+                carry += int(a.pop())
+            if b:
+                carry += int(b.pop())
+            
+            result += str(carry%2)
+            carry //= 2
+
+        return result[::-1]
